e4ed485a62c0611fa0864946075aa960e18a5679
louchrg <rosslouch@yahoo.com>
2017-01-28 17:20:43 -0600

    Added code files

---
+++ WebApplication1/Default.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebApplication1.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+	<title></title>
+</head>
+<body>
+	<form id="form1" runat="server">
+		<div>
+						<div><% Response.Write(GetPageHTML("http://gocomics.com/luann", ".item-comic-image")); %></div>
+			<div><% Response.Write(GetPageHTML("http://gocomics.com/wizardofid", ".item-comic-image")); %></div>
+			<div><% Response.Write(GetPageHTML("http://gocomics.com/pluggers", ".item-comic-image")); %></div>
+			<div><% Response.Write(GetPageHTML("http://gocomics.com/pearlsbeforeswine", ".item-comic-image")); %></div>
+			<div><% Response.Write(GetPageHTML("http://gocomics.com/garfield", ".item-comic-image")); %></div>
+			<div><% Response.Write(GetPageHTML("http://gocomics.com/foxtrot", ".item-comic-image")); %></div>
+			<div><% Response.Write(GetPageHTML("http://gocomics.com/calvinandhobbes", ".item-comic-image")); %></div>
+			<div><% Response.Write(GetPageHTML("http://gocomics.com/the-born-loser", ".item-comic-image")); %></div>
+			<div><% Response.Write(GetPageHTML("http://gocomics.com/bc", ".item-comic-image")); %></div>
+			<div><% Response.Write(GetPageHTML("http://gocomics.com/adamathome", ".item-comic-image")); %></div>
+			<div><% Response.Write(GetPageHTML("http://www.dilbert.com", ".img-comic-link")); %></div>
+			<div><% Response.Write(GetPageHTMLID("http://www.shermanslagoon.com", "comic")); %></div>
+		</div>
+	</form>
+</body>
+</html>
---
+++ WebApplication1/Default.aspx.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Net;
+using System.IO;
+
+namespace WebApplication1
+{
+	public partial class Default : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		public string GetPageHTML(string url)
+		{
+			string PageHTML;
+			HttpWebRequest resq = (HttpWebRequest)WebRequest.Create(url);
+
+			WebResponse resp = resq.GetResponse();
+			StreamReader sr = new StreamReader(resp.GetResponseStream(), System.Text.Encoding.UTF8);
+			PageHTML = sr.ReadToEnd();
+			resp.Close();
+			return PageHTML;
+		}
+
+		public string GetPageHTML(string url, string region)
+		{
+			string PageHTML;
+			HttpWebRequest resq = (HttpWebRequest)WebRequest.Create(url);
+
+			WebResponse resp = resq.GetResponse();
+			StreamReader sr = new StreamReader(resp.GetResponseStream(), System.Text.Encoding.UTF8);
+			PageHTML = sr.ReadToEnd();
+			resp.Close();
+			//return PageHTML;
+
+
+			var doc = new HtmlAgilityPack.HtmlDocument();
+			doc.LoadHtml(PageHTML);
+
+			HtmlAgilityPack.HtmlNode node = doc.QuerySelector(region);
+
+			HtmlAgilityPack.HtmlNode DateNode = doc.QuerySelector(".comic-title-date");
+			string ComicDate = DateNode.InnerText;
+			ComicDate = ComicDate.Replace("            2017", "");
+			ComicDate = ComicDate.Replace("\n", "");
+			DateTime comicDate = Convert.ToDateTime(ComicDate);
+
+			if (comicDate.Date == DateTime.Now.Date)
+				return node.InnerHtml;
+			else
+				return "";
+		}
+
+		public string GetPageHTMLID(string url, string region)
+		{
+			string PageHTML;
+			HttpWebRequest resq = (HttpWebRequest)WebRequest.Create(url);
+
+			WebResponse resp = resq.GetResponse();
+			StreamReader sr = new StreamReader(resp.GetResponseStream(), System.Text.Encoding.UTF8);
+			PageHTML = sr.ReadToEnd();
+			resp.Close();
+			//return PageHTML;
+
+
+			var doc = new HtmlAgilityPack.HtmlDocument();
+			doc.LoadHtml(PageHTML);
+
+			HtmlAgilityPack.HtmlNode node = doc.GetElementbyId(region);
+
+			HtmlAgilityPack.HtmlNode DateNode = doc.GetElementbyId("comic-date");
+			string ComicDate = DateNode.InnerText;
+			DateTime comicDate = Convert.ToDateTime(ComicDate);
+
+			if (comicDate.Date == DateTime.Now.Date)
+				return node.InnerHtml;
+			else
+				return "";
+		}
+	}
+}
